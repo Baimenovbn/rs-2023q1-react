dd0ad89e449c538bbced29fc3c7d73036a185dd3
Baimenovbn <baibn@mail.ru>
2023-03-19 09:40:57 +0600

    feat: add card list

---
+++ src/components/cards/index.tsx
@@ -0,0 +1,46 @@
+import { PropsWithChildren } from 'react';
+import { RsThumbList } from '../thumbs';
+import {
+  CardButton,
+  CardFooter,
+  CardImg,
+  CardListWrapper,
+  CardTitle,
+  CardWrapper,
+  InfoWrapper,
+} from './styled';
+
+export interface CardModelView {
+  id: string;
+  img: string;
+  title: string;
+  addedBy: string;
+  journalName: string;
+  whoAdded: string[];
+}
+
+export const Card = ({ img, title, addedBy, journalName, whoAdded }: CardModelView) => (
+  <CardWrapper>
+    <CardImg src={img} alt={title} />
+    <InfoWrapper>
+      <div>
+        <CardTitle>{title}</CardTitle>
+        <p>
+          Added by {addedBy} in {journalName}
+        </p>
+      </div>
+      <CardFooter>
+        <RsThumbList thumbList={whoAdded} />
+        <CardButton>Add</CardButton>
+      </CardFooter>
+    </InfoWrapper>
+  </CardWrapper>
+);
+
+export const CardList = ({ cardList }: PropsWithChildren<{ cardList: CardModelView[] }>) => (
+  <CardListWrapper>
+    {cardList.map((card) => (
+      <Card {...card} key={card.id} />
+    ))}
+  </CardListWrapper>
+);
--- src/pages/main/index.tsx
+++ src/pages/main/index.tsx
@@ -1,14 +1,33 @@
-import { SearchBar } from '../../components';
-import { useCallback } from 'react';
+import { useCallback, useState } from 'react';
+
+import styled from 'styled-components';
+
+import { CardList, CardModelView, SearchBar } from '../../components';
+import { CARD_LIST_STUB } from '../../stub/cards';
+
+const Wrapper = styled.div`
+  padding: 20px;
+`;
+
+const CardListWrapper = styled.section`
+  margin-top: 50px;
+`;
 
 export default function MainPage() {
+  const [cardListView, setCardListView] = useState<CardModelView[]>(CARD_LIST_STUB);
+
   const search = useCallback((searchValue: string) => {
-    console.log(searchValue);
+    setCardListView(
+      CARD_LIST_STUB.filter(({ title }) => title.toLowerCase().includes(searchValue.toLowerCase()))
+    );
   }, []);
 
   return (
-    <div>
+    <Wrapper>
       <SearchBar search={search} />
-    </div>
+      <CardListWrapper>
+        <CardList cardList={cardListView} />
+      </CardListWrapper>
+    </Wrapper>
   );
 }
